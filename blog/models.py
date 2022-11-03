2f6c158317306222a03368351ac3c7b8bbc71308
Rassul Magauin <rassulmagauin@gmail.com>
2022-11-04 02:26:40 +0600

    asd

--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,8 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
-<<<<<<< HEAD
 
     date = models.DateField()
 
     def __str__(self):
         return self.title
-=======
-    
-    date = models.DateField()
->>>>>>> 3bdcdc1034400883ca1aba4725c23e0524c55afc
